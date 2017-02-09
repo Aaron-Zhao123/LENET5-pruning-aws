ec5690fd2493abc9f76ef9d48c044d81bca53fdf
Aaron Zhao <aaron@Aarons-MacBook-Pro.local>
2017-02-09 23:10:24 +0000

    fixed a bug in b thres

--- training_v5.py
+++ training_v5.py
@@ -358,13 +358,12 @@ def main(argv = None):
                     # if (training_cnt == 10):
                     if (accuracy_mean > 0.999 or epoch > 50):
                         print('Training ends')
-                        print("saving model ...")
                         test_accuracy = accuracy.eval({
                                 x: mnist.test.images[:],
                                 y: mnist.test.labels[:],
                                 keep_prob: 1.})
                         print('test accuracy is {}'.format(test_accuracy))
-                        if (test_accuracy > 0.99 or epoch >200):
+                        if (test_accuracy > 0.99 or epoch >50):
                             file_name = 'weights_log/'+'pcov'+str(pruning_cov)+'pfc'+str(pruning_fc)+'.pkl'
                             with open(file_name, 'wb') as f:
                                 pickle.dump((
