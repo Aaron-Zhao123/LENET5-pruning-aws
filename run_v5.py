1d0f92951a9b5de5d5cc47721f6fcbe26d495977
Aaron Zhao <aaron@Aarons-MacBook-Pro.local>
2017-02-09 23:11:19 +0000

    fixed a bug in b thres

--- run_v5.py
+++ run_v5.py
@@ -15,17 +15,17 @@ pfc = 95
 retrain = 0
 model_tag = 'pcov'+str(pcov)+'pfc'+str(pfc)
 while (count < 5):
+    pcov = pcov+1
+    pfc = pfc+1
     param = [
     ('-pcov',pcov),
     ('-pfc',pfc),
     ('-m',model_tag)
     ]
-    pcov = pcov+1
-    pfc = pfc+1
     acc = training_v5.main(param)
     model_tag = 'pcov'+str(pcov)+'pfc'+str(pfc)
     acc_list.append(acc)
     count = count + 1
     print (acc)
-    
+
 print('accuracy summary: {}'.format(acc_list))
